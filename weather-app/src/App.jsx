4f89e2ee4474e7a597c17fab56e5a2b80c45c2be
Dimitris Vamvakas <dimitrisvamva1@gmail.com>
2026-07-18 18:12:13 +0300

    Initial commit
    basic package installation, and search bar component

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+import SearchBar from "./components/SearchBar";
+
+function App() {
+
+    return (
+        <>
+            <h1>Weather App</h1>
+
+            <SearchBar />
+        </>
+    )
+
+}
+
+export default App
---
+++ weather-app/src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Enter city..."
+            />
+
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default SearchBar
