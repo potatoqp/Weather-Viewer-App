cd133ab14936fd93c8061f18be8eeede3a5623bf
Dimitris Vamvakas <dimitrisvamva1@gmail.com>
2026-07-20 15:22:58 +0300

    Completed functionality
    Added weather data, loading state and error handling

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,22 +1,45 @@
 import SearchBar from "./components/SearchBar";
 import { useState } from "react";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
 
     const [city, setCity] = useState("");
     const [weather, setWeather] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
-    async function getWeather() {
+   async function getWeather() {
+
     const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
+    setLoading(true);
+    setError("");
 
-    const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    try {
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
         );
 
-    const data = await response.json();
+        if (!response.ok) {
+            throw new Error("City not found");
+        }
 
-    setWeather(data);
+        const data = await response.json();
+
+        setWeather(data);
+
+    } catch (err) {
+
+        setWeather(null);
+        setError(err.message);
+
+    } finally {
+
+        setLoading(false);
+
+    }
 
 }
 
@@ -29,6 +52,10 @@ function App() {
                 setCity={setCity}
                 getWeather={getWeather}
             />
+
+            {loading && <p>Loading...</p>}
+
+            {error && <p>{error}</p>}
 
             {weather && <WeatherCard weather={weather} />}
 
--- weather-app/src/components/SearchBar.jsx
+++ weather-app/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 function SearchBar({ city, setCity, getWeather }) {
   return (
     <div>
